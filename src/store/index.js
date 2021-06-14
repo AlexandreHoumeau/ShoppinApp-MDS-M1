954177072f1c7bbed44f33aa78f3b43d94eede19
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-06-14 09:31:25 +0200

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,10 @@ import thunk from 'redux-thunk';
 import orderProduct from "../reducers/Order.Reducer";
 import cartReducer from "../reducers/Cart.Reducer";
 import productReducer from '../reducers/Product.Reducer'
+import authReducer from "../reducers/Auth.Reducer";
+import ErrorAuthReducer from "../reducers/Error.Auth.Reducer";
 
-const rootReducer = combineReducers({ cart: cartReducer, order: orderProduct, products: productReducer  });
+const rootReducer = combineReducers({ cart: cartReducer, order: orderProduct, products: productReducer, user: authReducer,authError: ErrorAuthReducer  });
 const configureStore = () => {
   return createStore(rootReducer, applyMiddleware(thunk));
 };
